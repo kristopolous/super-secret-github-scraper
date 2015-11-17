ebd12bf08f8ee11ab16f00f067f92c89ae151c3c
Chris McKenzie <kristopolous@yahoo.com>
2015-11-17 12:32:54 -0800

    output of page

--- graball.sh
+++ graball.sh
@@ -20,7 +20,8 @@ dorepo() {
 gitsearch() {
   for page in `seq 1 99`; do
     for lang in css Shell Objective-C cpp css php unknown python javascript ruby bash c; do
-    repo_list=`curl 'https://github.com/search?l='$lang'&o=desc&p='$page'&q=stars%3A%3E1&s=forks&type=Repositories' | grep -A 1 repo-list-name | grep href | awk -F \" ' { print $2 } ' ` 
+      echo ">>> " $lang $page
+      repo_list=`curl 'https://github.com/search?l='$lang'&o=desc&p='$page'&q=stars%3A%3E1&s=stars&type=Repositories' | grep -A 1 repo-list-name | grep href | awk -F \" ' { print $2 } ' ` 
       sleep 4
       for repo in $repo_list; do
         dorepo $repo
@@ -32,6 +33,7 @@ gitsearch() {
 gitprojects() {
   for range in daily weekly monthly; do
     for lang in css Objective-C bash ruby cpp css php unknown python javascript ruby bash c; do
+      echo ">>> " $lang $range
       repo_list=`curl 'https://github.com/trending?l='$lang'&since='$range | grep -A 1 repo-list-name | grep href | awk -F \" ' { print $2 } ' ` 
       for repo in $repo_list; do
         dorepo $repo
